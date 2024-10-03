f98c8d53e62eb4c277b09b3e2d1a056a4da7a4a3
vonteacher <vaughn.weiss@gmail.com>
2024-10-03 13:36:40 -0600

    Import pygame.locals

--- game.py
+++ game.py
@@ -1,16 +1,13 @@
 import pygame
+
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT
+)
+
 pygame.init()
-
-screen = pygame.display.set_mode([500, 500])
-
-running = True
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    
-    screen.fill((255,255,255))
-    pygame.draw.circle(screen, (0,0,255), (250, 250), 75)
-    pygame.display.flip()
-
-pygame.quit()
