5cdc63dbc402424c3a34df2b2683b0af4834957a
vonteacher <vaughn.weiss@gmail.com>
2024-10-17 14:48:32 -0600

    Rework player and block to sprites

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from pygame.locals import (
 )
 from random import randint, randrange
 
-STEP_SIZE = 10
+STEP_SIZE = 5
 SPRITE_SIZE = 40
 
 WIDTH_UNITS = 16
@@ -22,86 +22,81 @@ SCREEN_HEIGHT = HEIGHT_UNITS * SPRITE_SIZE
 
 MAX_BLOCK_COUNT = 20
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Player, self).__init__()
+        self.surface = pygame.Surface((SPRITE_SIZE, SPRITE_SIZE))
+        self.surface.fill((0, 255, 0))
+        self.rect = self.surface.get_rect(left = 0, top = 0)
+
+    def update(self, keys):
+        if keys[K_UP]:    playerRect.move_ip(0, -STEP_SIZE)
+        if keys[K_DOWN]:  playerRect.move_ip(0, STEP_SIZE)
+        if keys[K_LEFT]:  playerRect.move_ip(-STEP_SIZE, 0)
+        if keys[K_RIGHT]: playerRect.move_ip(STEP_SIZE, 0)
+
+        # Keep player on screen
+        if self.rect.top < 0: self.rect.top = 0
+        if self.rect.bottom > SCREEN_HEIGHT: self.rect.bottom = SCREEN_HEIGHT
+        if self.rect.left < 0: self.rect.left = 0
+        if self.rect.right > SCREEN_WIDTH: self.rect.right = SCREEN_WIDTH
+
+class Block(pygame.sprite.Sprite):
+    def __init__(self):
+        super(Block, self).__init__()
+        self.surface = pygame.Surface((SPRITE_SIZE, SPRITE_SIZE))
+        self.surface.fill((255, 0, 0))
+        self.rect = self.surface.get_rect(
+            left = randrange(WIDTH_UNITS) * SPRITE_SIZE,
+            top = randrange(HEIGHT_UNITS) * SPRITE_SIZE)
+
 pygame.init()
-
-clock  = pygame.time.Clock()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = pygame.Rect(0, 0, SPRITE_SIZE, SPRITE_SIZE)
-blocks = []
+clock  = pygame.time.Clock()
 
+player = Player()
+playerRect = player.rect
+
+blocks = pygame.sprite.Group()
 while len(blocks) < MAX_BLOCK_COUNT:
-    block = pygame.Rect(
-        randrange(WIDTH_UNITS) * SPRITE_SIZE,
-        randrange(HEIGHT_UNITS) * SPRITE_SIZE,
-        SPRITE_SIZE,
-        SPRITE_SIZE
-    )
-    if any([(block.top == existing.top and block.left == existing.left) or (block.top == 0 and block.left == 0) for existing in blocks]): continue
-    else: blocks.append(block)
+    block = Block()
+    if any(
+        [
+            (block.rect.top == existing.rect.top and block.rect.left == existing.rect.left) or \
+            (block.rect.top == 0 and block.rect.left == 0) \
+            for existing in blocks
+        ]
+    ): continue
+    else: blocks.add(block)
+all_sprites = pygame.sprite.Group()
+all_sprites.add(player)
+all_sprites.add(blocks)
 
 running = True
 
 while running:
-    px = player.left
-    # px1 = player.left + SPRITE_SIZE
-    px1 = player.right
-    py = player.top
-    # py1 = player.top + SPRITE_SIZE
-    py1 = player.bottom
+    px = playerRect.left
+    px1 = playerRect.right
+    py = playerRect.top
+    py1 = playerRect.bottom
 
     for event in pygame.event.get():
         if event.type == KEYDOWN:
             if event.key == K_ESCAPE or event.key == K_q: running = False # Presse ESCAPE key
         if event.type == QUIT: running = False # Click X on the window
 
-        keys = pygame.key.get_pressed()
+    keys = pygame.key.get_pressed()
 
-    if keys[K_UP]:    player.top -= STEP_SIZE
-    if keys[K_DOWN]:  player.top += STEP_SIZE
-    if keys[K_LEFT]:  player.left -= STEP_SIZE
-    if keys[K_RIGHT]: player.left += STEP_SIZE
-
-    for b in blocks:
-        bx  = b.left
-        # bx1 = b.left + SPRITE_SIZE
-        bx1 = b.right
-        by  = b.top
-        # by1 = b.top + SPRITE_SIZE
-        by1 = b.bottom
-
-        # pygame/src_c/rect.c
-        # https://github.com/pygame/pygame/blob/79807da84c9bacf8df5a177763e14c924e3b15e2/src_c/rect.c#L356
-        if player.colliderect(b):
-            STEP_SIZE = 0
-            # The < | > check allows the player to pass through the block
-            # on a well-placed diagonal collision.
-            # SOLUTION: should be to change to <= and >=
-
-            # Second bug: if two blockers are next to each other, when the player
-            # slides from one to the other, the player gets stuck on the second
-            # blocks corner
-            if (px < bx1 and px1 > bx) and py == by1: player.top = by1
-            if (px < bx1 and px1 > bx) and py1 == by: player.top = by - SPRITE_SIZE
-            if (py < by1 and py1 > by) and px == bx1: player.left = bx1
-            if (py < by1 and py1 > by) and px1 == bx: player.left = bx - SPRITE_SIZE
-            # blocks.remove(b)
-            # SCORE += 1
-            # Once the last block was removed, I was unable to move the player. Why is it happening?
-        else: STEP_SIZE = 5
-
-        # BUG: if we hit the corners just perfectly so, the collision detection fails and
-        # the player block goes through a red block
-
-    # Keep player on screen
-    if player.top < 0: player.top = 0
-    if player.top > SCREEN_HEIGHT - SPRITE_SIZE: player.top = SCREEN_HEIGHT - SPRITE_SIZE
-    if player.left < 0: player.left = 0
-    if player.left > SCREEN_WIDTH - SPRITE_SIZE: player.left = SCREEN_WIDTH - SPRITE_SIZE
+    player.update(keys)
 
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (0, 255, 0), player)
-    for b in blocks: pygame.draw.rect(screen, (255, 0, 0), b)
+    for entity in all_sprites:
+        screen.blit(entity.surface, entity.rect)
+
+    if pygame.sprite.spritecollideany(player, blocks):
+        player.kill()
+        running = False
 
     pygame.display.flip()
 
